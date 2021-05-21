198d0a500beeede44cc1208e5ae789524037b70a
Shreyans Shah <shahshreyans98@gmail.com>
2021-05-21 10:47:35 +0530

    Day-1/ Django instllation Done

---
+++ Connecting_Humanity/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Connecting_Humanity project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Connecting_Humanity.settings')
+
+application = get_wsgi_application()
